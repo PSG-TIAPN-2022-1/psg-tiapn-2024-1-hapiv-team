318211554abdbd70b1af3b307482c41abc578ae7
IgorLucasAssuncao <igorlucas1217@gmail.com>
2024-05-28 22:13:06 -0300

    Construindo Controllers

---
+++ codigo/backend/HapivAPI/Constantes/ConstantesGerais.cs
@@ -0,0 +1,10 @@
+﻿namespace HapivAPI.Constantes
+{
+    public static class ConstantesGerais
+    {
+        public static class Controllers
+        {
+            public static readonly string SemCategorias = "Ainda não houve nenhuma categoria cadastrada!";
+        }
+    }
+}
