9e8d8dd2a5181a4728b8fff63f11e4372781a215
Hadassa Brenda <hadassa.brenda@sga.pucminas.br>
2024-06-17 10:31:47 -0300

    Logica para o button ficar no final da tela do modal

--- codigo/frontEnd/emporio/src/screens/BaseScreen/components/TelaEstoque/components/Modal/modal.style.js
+++ codigo/frontEnd/emporio/src/screens/BaseScreen/components/TelaEstoque/components/Modal/modal.style.js
@@ -14,12 +14,16 @@ export const ModalContainer = styled.div`
   background-color: rgba(0,0,0,0.5);
 `;
 export const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   margin: auto;
   padding: 20px;
   border: 2px solid #888;
-  width: 30%; 
+  width: 90%; 
+  overflow-y: auto; 
   border-radius: 10px;
-   background-color: #FBF2EF;
+  background-color: rgb(245, 245, 220);
 `;
 export const Title = styled.h2`
   text-align: center;
