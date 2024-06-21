6997d8cb820aa244466c227181b5ccda58e5490b
Abner Mateus de Oliveira Santos <mateusabner0@gmail.com>
2024-06-20 21:44:46 -0300

    Renomeia componente modal

--- codigo/frontEnd/emporio/src/screens/BaseScreen/components/TelaEstoque/components/Modal/modal.jsx
+++ codigo/frontEnd/emporio/src/screens/BaseScreen/components/TelaEstoque/components/Modal/modal.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 import {
   ModalContainer,
   ModalContent,
   Title,
   Content,
   Buttons,
-} from "./modal.style"
-import { Button } from "../Button/Button"
+} from "./Modal.style";
+import { Button } from "../Button/Button";
 
 export default function Modal({ isOpen, setModalOpen, title, children }) {
   if (!isOpen) {
@@ -19,8 +19,8 @@ export default function Modal({ isOpen, setModalOpen, title, children }) {
         <Title>{title}</Title>
         <Content>{children}</Content>
         <Buttons>
-        <Button title="Close" onClick={() => setModalOpen(false)} />
-        <Button title={"Salvar"}/>
+          <Button title="Close" onClick={() => setModalOpen(false)} />
+          <Button title={"Salvar"} />
         </Buttons>
       </ModalContent>
     </ModalContainer>
