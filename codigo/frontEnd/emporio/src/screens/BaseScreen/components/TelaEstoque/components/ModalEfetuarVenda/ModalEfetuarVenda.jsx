72061d7a1162bddf19a5e990166fae986aaa3c59
Hadassa Brenda <hadassa.brenda@sga.pucminas.br>
2024-06-30 17:01:09 -0300

    Atualização - Efetuar Venda

--- codigo/frontEnd/emporio/src/screens/BaseScreen/components/TelaEstoque/components/ModalEfetuarVenda/ModalEfetuarVenda.jsx
+++ codigo/frontEnd/emporio/src/screens/BaseScreen/components/TelaEstoque/components/ModalEfetuarVenda/ModalEfetuarVenda.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "../Modal/Modal";
+import { ContainerModalEfetuarVenda } from "./ModalEfetuarVenda.style";
+import { InputModal } from "../Modal/components/InputModal/InputModal";
 
-export const ModalEfetuarVenda = ({ estahAberto, setAberto }) => (
-  <Modal
-    estahAberto={estahAberto}
-    setAberto={setAberto}
-    titulo="Efetuar Venda"
-    conteudo={<></>}
-  />
-);
+export const ModalEfetuarVenda = ({
+  estahAberto,
+  setAberto,
+  onProdutoAdicionado,
+}) => {
+  const [quantidade, setQuantidade] = useState("");
+
+  return (
+    <Modal
+      estahAberto={estahAberto}
+      setAberto={setAberto}
+      titulo="Efetuar Venda"
+      conteudo={
+        <ContainerModalEfetuarVenda>
+          <InputModal
+            type="text"
+            title="Quantidade"
+            placeholder={"Quantidade de Produto"}
+            onChange={(e) => setQuantidade(e.target.value)}
+          />
+        </ContainerModalEfetuarVenda>
+      }
+    />
+  );
+};
---
+++ codigo/frontEnd/emporio/src/screens/BaseScreen/components/TelaEstoque/components/ModalEfetuarVenda/ModalEfetuarVenda.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ContainerModalEfetuarVenda = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
